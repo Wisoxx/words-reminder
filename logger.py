571e34889426fe627315a8a3294034b1784352e5
Wisoxx <bohdantavanov@gmail.com>
2024-10-08 18:21:58 +0200

    specified encoding for logs

--- logger.py
+++ logger.py
@@ -7,7 +7,7 @@ def setup_logger(name):
     log_path = os.path.join(os.path.expanduser("~"), 'mysite', 'logs')
     os.makedirs(log_path, exist_ok=True)
 
-    handler = RotatingFileHandler(os.path.join(log_path, 'app.log'), maxBytes=10000000, backupCount=3)
+    handler = RotatingFileHandler(os.path.join(log_path, 'app.log'), maxBytes=10000000, backupCount=3, encoding='utf-8')
     formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]',
                                   datefmt='%Y-%m-%d %H:%M:%S')
     handler.setFormatter(formatter)
