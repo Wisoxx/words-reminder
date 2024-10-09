e05e8cca88580761736492c7afbc7dcdb6483e98
Wisoxx <bohdantavanov@gmail.com>
2024-10-09 20:05:22 +0200

    set simultaneous logging to console and file

--- logger.py
+++ logger.py
@@ -3,24 +3,25 @@ from logging.handlers import RotatingFileHandler
 import os
 
 
-def setup_logger(name, log_to_file=False):
+def setup_logger(name):
     log_path = os.path.join(os.path.expanduser("~"), 'mysite', 'logs')
     os.makedirs(log_path, exist_ok=True)
 
-    if log_to_file:
-        handler = RotatingFileHandler(os.path.join(log_path, 'app.log'), maxBytes=10000000, backupCount=3, encoding='utf-8')
-    else:
-        handler = logging.StreamHandler()
-
+    file_handler = RotatingFileHandler(os.path.join(log_path, 'app.log'), maxBytes=1000000, backupCount=4)
     formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]',
                                   datefmt='%Y-%m-%d %H:%M:%S')
-    handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_formatter = logging.Formatter('%(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    console_handler.setFormatter(console_formatter)
 
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
     if logger.hasHandlers():
         logger.handlers.clear()
-    logger.addHandler(handler)
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
 
     return logger
