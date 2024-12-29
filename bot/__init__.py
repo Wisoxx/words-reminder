2db8c14d9e8fda473281a3cd4e4107366f0f0ed1
Wisoxx <bohdantavanov@gmail.com>
2024-12-29 21:07:08 +0100

    added logs to broadcasting, improved update handling

--- bot/__init__.py
+++ bot/__init__.py
@@ -64,15 +64,28 @@ class Bot:
         if add_cancel_button:
             self.manage_cancel_buttons(user, response.get('message_id'), delete_old=False)  # old deleted in get_user
 
-    def broadcast(self, text: str, reply_markup=None):
+    def broadcast(self, text: str, reply_markup=None, exceptions=None):
+        logger.info("Broadcasting message: {}".format(text))
+        exceptions = exceptions or []
+        logger.info(f"Found {len(exceptions)} exceptions")
         users = db.Users.execute_query("SELECT user_id FROM users;")
         for user in users:
+            if user[0] in exceptions:
+                continue
             self.deliver_message(user[0], text, reply_markup=reply_markup)
+        logger.info(f"Sent to {len(users)} users")
 
-    def broadcast_multilang(self, text: dict, reply_markup=None):
+    def broadcast_multilang(self, text: dict, reply_markup=None, exceptions=None):
+        logger.info("Broadcasting message: {}".format(text))
+        exceptions = exceptions or []
+        logger.info(f"Found {len(exceptions)} exceptions")
         users = db.Users.execute_query("SELECT user_id, language FROM users;")
         for user, lang in users:
+            if user[0] in exceptions:
+                continue
             self.deliver_message(user, text[lang], reply_markup=reply_markup)
+        logger.info(f"Sent to {len(users)} users")
+
 
     def get_user_parameters(self, user):
         if user in self.users_data:
@@ -85,8 +98,13 @@ class Bot:
     def get_user(self, update):
         if "message" in update:
             user = update["message"]["chat"]["id"]
-        elif "callback_query" in update:
-            user = update["callback_query"]["message"]["chat"]["id"]
+            lang = update["message"]["from"]["language_code"]
+        elif "inline_query" in update:
+            user = update["inline_query"]["from"]["id"]
+            lang = update["inline_query"]["from"]["language_code"]
+        elif "my_chat_member" in update:
+            user = update["my_chat_member"]["from"]["id"]
+            lang = update["my_chat_member"]["from"]["language_code"]
         else:
             raise KeyError("Couldn't find user")
 
@@ -94,23 +112,33 @@ class Bot:
             self.get_user_parameters(user)
 
         self.manage_cancel_buttons(user)
-        return user
+        return user, lang
 
     def handle_update(self, update):
         user = None
+        lang = None
         try:
-            logger.debug('Received update: {}'.format(update))
+            logger.debug('Received update: {}'.format(json.dumps(update, indent=4)))  # pretty print logs
 
-            user = self.get_user(update)
+            user, lang = self.get_user(update)
 
             if "message" in update:
-                self.handle_message(user, update)
+                self.handle_message(user, lang, update)
 
             elif "callback_query" in update:
-                self.handle_callback_query(user, update)
+                self.handle_callback_query(user, lang, update)
+
+            elif "my_chat_member" in update:
+                self.handle_chat_member_status(user, lang, update)
 
         except Exception as e:
             logger.critical(f"Couldn't process update: {e}", exc_info=True)
+            logger.critical(f"Update that caused error: {json.dumps(update, indent=4)}")
 
-            if user:
-                self.deliver_message(user, "Something went wrong")
+            if user and lang:
+                try:
+                    self.deliver_message(user, translate(lang, "error"))
+                except Exception as e_:
+                    logger.critical(f"Couldn't notify user {user} about error: {e_}")
+                else:
+                    logger.info(f"User {user} has been notified about error")
--- bot/_handlers.py
+++ bot/_handlers.py
@@ -1,4 +1,4 @@
-def handle_message(self, user, update):
+def handle_message(self, user, lang, update):
     if "text" in update["message"]:
         text = update["message"]["text"]
         match text:
@@ -15,6 +15,10 @@ def handle_message(self, user, update):
         self.deliver_message(user, "From the web: sorry, I didn't understand that kind of message")
 
 
-def handle_callback_query(self, user, update):
+def handle_callback_query(self, user, lang, update):
     callback_data = update["callback_query"]["data"]
     self.deliver_message(user, callback_data)
+
+
+def handle_chat_member_status(self, user, lang, update):
+    raise Exception("Blocked or unblocked")
