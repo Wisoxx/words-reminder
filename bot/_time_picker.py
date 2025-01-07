a5df25786c22501c7463d7a46bf8cc8b28e0ee72
Wisoxx <bohdantavanov@gmail.com>
2025-01-07 18:44:48 +0100

    and minutes buttons are optional

--- bot/_time_picker.py
+++ bot/_time_picker.py
@@ -16,7 +16,21 @@ def test(update):
         inline_keyboard=[
             [
                 InlineKeyboardButton(text="PICK TIME",
-                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value])),
+                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value, True])),
+            ],
+        ]
+    )
+    return text, reply_markup
+
+
+@route(trigger="text", command="/test2", action="send")
+def test2(update):
+    text = "Test"
+    reply_markup = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text="PICK TIME",
+                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value, False])),
             ],
         ]
     )
@@ -29,43 +43,74 @@ def pick_time(update):
     parameters = get_user_parameters(user)
     timezone = parameters.timezone
     callback_data = json.loads(update["callback_query"]["data"])
-    time = callback_data[1] if len(callback_data) > 1 else suggest_reminder_time()
-    reply_markup = InlineKeyboardMarkup(
-        inline_keyboard=[
+    include_minutes = callback_data[1]
+    time = callback_data[2] if len(callback_data) > 2 else suggest_reminder_time()
+    rows = [
+        [
+            InlineKeyboardButton(text=shift_time(time, hour_offset=timezone),
+                                 callback_data=json.dumps([QUERY_ACTIONS.TIME_CHOSEN.value, time])),
+        ],
+        [
+            InlineKeyboardButton(text="  -5 год  ",
+                                 callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value,
+                                                           include_minutes,
+                                                           shift_time(time, -5)])),
+            InlineKeyboardButton(text="  -1 год  ",
+                                 callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value,
+                                                           include_minutes,
+                                                           shift_time(time, -1)])),
+            InlineKeyboardButton(text="  +1 год  ",
+                                 callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value,
+                                                           include_minutes,
+                                                           shift_time(time, 1)])),
+            InlineKeyboardButton(text="  +5 год  ",
+                                 callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value,
+                                                           include_minutes,
+                                                           shift_time(time, 5)])),
+        ],
+    ]
+
+    if include_minutes:
+        rows.extend(
             [
-                InlineKeyboardButton(text=shift_time(time, hour_offset=timezone),
-                                     callback_data=json.dumps([QUERY_ACTIONS.TIME_CHOSEN.value, time])),
-            ],
-            [
-                InlineKeyboardButton(text="  -5 год  ",
-                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value, shift_time(time, -5)])),
-                InlineKeyboardButton(text="  -1 год  ",
-                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value, shift_time(time, -1)])),
-                InlineKeyboardButton(text="  +1 год  ",
-                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value, shift_time(time, 1)])),
-                InlineKeyboardButton(text="  +5 год  ",
-                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value, shift_time(time, 5)])),
-            ],
-            [
-                InlineKeyboardButton(text="  -5 хв  ",
-                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value, shift_time(time, 0, -5)])),
-                InlineKeyboardButton(text="  -1 хв  ",
-                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value, shift_time(time, 0, -1)])),
-                InlineKeyboardButton(text="  +1 хв  ",
-                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value, shift_time(time, 0, 1)])),
-                InlineKeyboardButton(text="  +5 хв  ",
-                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value, shift_time(time, 0, 5)])),
-            ],
-            [
-                InlineKeyboardButton(text="  -15 хв  ",
-                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value, shift_time(time, 0, -15)])),
-                InlineKeyboardButton(text="  -10 хв  ",
-                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value, shift_time(time, 0, -10)])),
-                InlineKeyboardButton(text="  +10 хв  ",
-                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value, shift_time(time, 0, 10)])),
-                InlineKeyboardButton(text="  +15 хв  ",
-                                     callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value, shift_time(time, 0, 15)])),
-            ],
-        ]
-    )
+                [
+                    InlineKeyboardButton(text="  -5 хв  ",
+                                         callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value,
+                                                                   include_minutes,
+                                                                   shift_time(time, 0, -5)])),
+                    InlineKeyboardButton(text="  -1 хв  ",
+                                         callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value,
+                                                                   include_minutes,
+                                                                   shift_time(time, 0, -1)])),
+                    InlineKeyboardButton(text="  +1 хв  ",
+                                         callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value,
+                                                                   include_minutes,
+                                                                   shift_time(time, 0, 1)])),
+                    InlineKeyboardButton(text="  +5 хв  ",
+                                         callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value,
+                                                                   include_minutes,
+                                                                   shift_time(time, 0, 5)])),
+                ],
+                [
+                    InlineKeyboardButton(text="  -15 хв  ",
+                                         callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value,
+                                                                   include_minutes,
+                                                                   shift_time(time, 0, -15)])),
+                    InlineKeyboardButton(text="  -10 хв  ",
+                                         callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value,
+                                                                   include_minutes,
+                                                                   shift_time(time, 0, -10)])),
+                    InlineKeyboardButton(text="  +10 хв  ",
+                                         callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value,
+                                                                   include_minutes,
+                                                                   shift_time(time, 0, 10)])),
+                    InlineKeyboardButton(text="  +15 хв  ",
+                                         callback_data=json.dumps([QUERY_ACTIONS.PICK_TIME.value,
+                                                                   include_minutes,
+                                                                   shift_time(time, 0, 15)])),
+                ],
+            ]
+        )
+
+    reply_markup = InlineKeyboardMarkup(inline_keyboard=rows)
     return reply_markup
