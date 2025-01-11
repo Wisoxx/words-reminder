ef231c4d8da2e6d3a4d51acf5743a37b297f4f9e
Wisoxx <bohdantavanov@gmail.com>
2025-01-11 21:18:47 +0100

    added back missing action

--- bot/_enums.py
+++ bot/_enums.py
@@ -37,6 +37,7 @@ class QUERY_ACTIONS(Enum):
     TOGGLE_HIDE_MEANING = auto()
     CHANGE_TIMEZONE = auto()
     CHANGE_TIMEZONE_FINALIZE = auto()
+    PICK_TIME = auto()
 
 
 class TEMP_KEYS(Enum):
